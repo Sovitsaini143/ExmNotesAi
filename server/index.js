1681678f23af0b50f31a4fe2761f546373fc1383
Sovit saini <sovitsaini37@gmail.com>
2026-03-27 15:26:28 +0530

    super

--- server/index.js
+++ server/index.js
@@ -1,53 +1,52 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './utils/connectDB.js'
-import authRouter from './routes/auth.route.js'
-
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import userRouter from './routes/user.route.js'
-import notesRouter from './routes/generate.route.js'
-import pdfRouter from './routes/pdf.route.js'
-import creditRouter from './routes/credits.route.js'
+import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./utils/connectDb.js"
+import authRouter from "./routes/auth.route.js"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import userRouter from "./routes/user.route.js"
+import notesRouter from "./routes/genrate.route.js"
+import pdfRouter from "./routes/pdf.route.js"
+import creditRouter from "./routes/credits.route.js"
 import { stripeWebhook } from "./controllers/credits.controller.js"
 dotenv.config()
 
 
 
+
 const app = express()
+
 app.post(
   "/api/credits/webhook",
   express.raw({ type: "application/json" }),
   stripeWebhook
 );
 
-
 app.use(cors(
-    {origin: "https://exmnotesaiclient.onrender.com",
+    {origin:"https://exmnotesaiclient.onrender.com",
         credentials:true,
-        methods: ["GET", "POST", "PUT", 'DELETE', 'OPTIONS']
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
-));
-
-app.use(express.json());
-app.use(cookieParser());
+))
 
 
 
+app.use(express.json())
+app.use(cookieParser())
+const PORT = process.env.PORT || 5000
+app.get("/",(req,res)=>{
+    res.json({message:"ExamNotes AI Backend Running 🚀"})
 
-const PORT = process.env.PORT || 7000
-
-
-
-
-
-app.use("/api/auth", authRouter)
+})
+app.use("/api/auth" , authRouter)
 app.use("/api/user", userRouter)
 app.use("/api/notes", notesRouter)
-app.use("/api/pdf", pdfRouter )
-app.use("/api/credit", creditRouter )
+app.use("/api/pdf", pdfRouter)
+app.use("/api/credit",creditRouter)
 
-app.listen(PORT, () => { 
-    console.log(`server is running on port ${PORT}`)
-    connectDB()
+
+
+app.listen(PORT,()=>{
+    console.log(`✅ Server running on port ${PORT}`)
+    connectDb()
 })
