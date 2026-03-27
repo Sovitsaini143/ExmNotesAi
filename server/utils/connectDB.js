197f8aa34407840cee3b2d8441aed9ea80a36099
Sovit saini <sovitsaini37@gmail.com>
2026-03-27 15:30:22 +0530

    doneeee

--- server/utils/connectDB.js
+++ server/utils/connectDB.js
@@ -1,13 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectDB = async () => {
-    try{
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("DB Connected successfully")
-
-    } catch (error){
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("DB Connected")
+    } catch (error) {
         console.log("DB Error")
     }
 }
-
-export default connectDB
+export default connectDb
